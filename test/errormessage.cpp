f8270aee59aec2317ac1210f3f900523961bee8e
Markus Lohse <markus.lohse@bausch.com>
2013-07-01 19:45:20 +0200

    fix bug parsing error message

--- test/errormessage.cpp
+++ test/errormessage.cpp
@@ -11,12 +11,11 @@ ErrorMessage::ErrorMessage(quint8 seq, ErrorCode errorCode, quint8 seqError) :
 ErrorMessage::ErrorMessage(const QByteArray &rawData) :
 	AbstractMessage(rawData.size() > 4 ? static_cast<quint8>(rawData.at(2)) : 0)
 {
-	if (rawData.size() == 5 || rawData.size() == 6)
 	{
-		m_data.append(rawData.mid(4, rawData.size() - 4));
+		m_data.append(rawData.mid(3, rawData.size() - 4));
 	}
 
-	if (rawData.at(0) != identifier() || static_cast<quint8>(rawData.at(rawData.size() - 1)) != checksum())
+	if (rawData.at(0) != identifier() || (static_cast<quint8>(rawData.at(rawData.size() - 1)) != checksum()))
 	{
 		m_data.clear(); // invalidate message
 	}
